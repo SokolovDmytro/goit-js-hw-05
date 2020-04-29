e79900a9ae85483bf5c38ccbf4e8ae3e9d0c75f9
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-04-29 18:02:10 +0300

    fixed task-01

--- js/task-02.js
+++ js/task-02.js
@@ -1,39 +1,40 @@
-'use strict';
+// 'use strict';
 
-// Напиши класс User для создания пользователя со следующим свойствами:
+// // Напиши класс User для создания пользователя со следующим свойствами:
 
-// name - строка
-// age - число
-// followers - число
-// Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+// // name - строка
+// // age - число
+// // followers - число
+// // Добавь метод getInfo(), который,
+// //  выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
 
-class User {
-  constructor({
-    name,
-    age,
-    followers
-  }) {
-    this.name = name;
-    this.age = age;
-    this.followers = followers;
-  }
-  getInfo() {
-    return console.log(
-      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
-  }
-}
-  const mango = new User({
-    name: 'Mango',
-    age: 2,
-    followers: 20,
-  });
+// class User {
+//   constructor({
+//     name,
+//     age,
+//     followers
+//   }) {
+//     this.name = name;
+//     this.age = age;
+//     this.followers = followers;
+//   }
+//   getInfo() {
+//     return console.log(
+//       `User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+//   }
+// }
+//   const mango = new User({
+//     name: 'Mango',
+//     age: 2,
+//     followers: 20,
+//   });
 
-  mango.getInfo(); // User Mango is 2 years old and has 20 followers
+//   mango.getInfo(); // User Mango is 2 years old and has 20 followers
 
-  const poly = new User({
-    name: 'Poly',
-    age: 3,
-    followers: 17,
-  });
+//   const poly = new User({
+//     name: 'Poly',
+//     age: 3,
+//     followers: 17,
+//   });
 
-  poly.getInfo(); // User Poly is 3 years old and has 17 followers
+//   poly.getInfo(); // User Poly is 3 years old and has 17 followers
