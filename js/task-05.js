3a959dccbd9eee366fa9e90b65b6c6ddc9fce715
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-05-05 18:55:09 +0300

    fixed task-05

--- js/task-05.js
+++ js/task-05.js
@@ -66,21 +66,17 @@ class Car {
   // не больше чем значение свойства maxSpeed
 
   accelerate(value) {
-    if (this.speed + value > this.maxSpeed) {
+    if (this.speed + value >= this.maxSpeed) return;
 
-    } 
-    else {
-      this.speed += Number(value);
-    }
+    this.speed = this.speed + value;
   }
+
   // Отнимает от свойства speed полученное значение,
   // при условии что результирующая скорость не меньше нуля
 
   decelerate(value) {
-    if (this.speed - value < 0) {
-
-    } else {
-      this.speed -= Number(value);
+    if (this.speed - value >= 0) {
+      this.speed -= value;
     }
   }
 
@@ -93,7 +89,6 @@ class Car {
     }
   }
 }
-
 
 const mustang = new Car({
   maxSpeed: 200,
